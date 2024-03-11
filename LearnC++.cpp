a778e7ac6f19a53519a8744807e2009867d2fb1a
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-03-11 12:03:23 +0530

    first commit

---
+++ LearnC++.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void something(int arrr[]){
+
+
+}
+
+
+int main(){
+
+
+    int n =5;
+    int arr[n];
+
+    for (int i =0 ; i <= n-1 ; i=i+ 1){
+
+        cin>> arr[i];
+    }
+ 
+return 0;
+
+}
