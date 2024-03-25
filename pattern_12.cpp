6e729c1adab24f52a0e5c0e4077ce18bef76f158
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-03-25 11:00:17 +0530

    Add Pattern 12

---
+++ pattern_12.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print12(int n){
+    int space = 2* (n-1);
+    //outer loop
+    for( int i =1 ; i<=n;i++){
+        //numbers
+        for(int j = 1 ; j<=i;j++){
+            cout<<j;
+        }
+        //space
+        for(int j = 1; j<=space;j++){
+            cout<<" ";
+        }
+        //numbers
+        for(int j =i ; j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+        space-=2;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    print12(n);
+}
